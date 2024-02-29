70f8fd5f9111956c3cce7f3565697f022c25a573
Julian Chu <75326388+juliankcchu@users.noreply.github.com>
2024-02-29 19:02:03 +0800

    Create bank-account-project-part-1.kt

---
+++ fundamentals-kotlin/bank-account-project-part-1.kt
@@ -0,0 +1,27 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+  println("Welcome to your banking system.")
+	println("What type of account would you like to create?")
+	println("1. Debit account")
+	println("2. Credit account")
+	println("3. Checking account")
+    
+    var accountType = ""
+    var userChoice = 0
+    
+    while (accountType == "") {
+	    println("Choose an option (1, 2 or 3)")
+        userChoice = (1..5).random()
+        println("The selected option is..." + userChoice)
+        when {
+            userChoice == 1 -> accountType = "debit"
+            userChoice == 2 -> accountType = "credit"
+            userChoice == 3 -> accountType = "checking"
+            else -> accountType = ""            
+        }
+    }
+    println("You have created a $accountType account.")
+}
