da22d7ea3cf9a61c71c587880b8eeb81fad9330d
Julian Chu <75326388+juliankcchu@users.noreply.github.com>
2024-05-21 13:12:37 +0800

    Update LittleLemonFooter.js

--- course10/first-rn-component/components/LittleLemonFooter.js
+++ course10/first-rn-component/components/LittleLemonFooter.js
@@ -3,8 +3,17 @@ import { View, Text } from 'react-native';
 
 export default function LittleLemonFooter() {
   return (
-    <View>
-      <Text>
+    <View
+      style={{
+        backgroundColor: '#F4CE14',
+        marginBottom: 10,
+      }}>
+      <Text
+        style={{
+          fontSize: 18,
+          color: 'black',
+          textAlign: 'center',
+        }}>
         All rights reserved by Little Lemon, 2022{' '}
       </Text>
     </View>
